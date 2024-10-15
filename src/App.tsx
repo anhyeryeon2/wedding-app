0c22d8f0d6930040ef5a5ac22693d833bca66bd5
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-10-15 21:53:59 +0900

    feat:imageGallery with swiper

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React, { useEffect, useState } from 'react'
 import classNames from 'classnames/bind'
-import styles from './App.module.scss'
-import FullScreenMessage from './components/shared/FullScreenMessage'
+import { useEffect, useState } from 'react'
 
+import styles from './App.module.scss'
 import Heading from './components/sections/Heading'
 import Video from './components/sections/Video'
 
-import {Wedding} from  '@models/wedding'
+import FullScreenMessage from './components/shared/FullScreenMessage'
+import { Wedding } from '@models/wedding'
+import ImageGallery from './components/sections/ImageGallery'
+
 const cx = classNames.bind(styles)
 
 function App() {
-  const [wedding,setWedding]= useState<Wedding | null>(null);
-  const[loading,setLoading]= useState(false);
-  const [error,setError]= useState(false);
-  useEffect(()=>{
+  const [wedding, setWedding] = useState<Wedding | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  // 1. wedding 데이터 호출
+  useEffect(() => {
     setLoading(true)
+
     fetch('http://localhost:8888/wedding')
       .then((response) => {
         if (response.ok === false) {
-          throw new Error('청첩장 정보를 불러오지 못했습니다. ')
+          throw new Error('청첩장 정보를 불러오지 못했습니다.')
         }
+
         return response.json()
       })
       .then((data) => {
@@ -32,24 +38,27 @@ function App() {
       .finally(() => {
         setLoading(false)
       })
-  },[])
+  }, [])
 
-  if(loading){
-    return <div><FullScreenMessage type='loading'/> </div>
+  if (loading) {
+    return <FullScreenMessage type="loading" />
   }
-  if(error){
-    return  <div><FullScreenMessage type='error'/> </div>
+
+  if (error) {
+    return <FullScreenMessage type="error" />
   }
-  if(wedding == null ){
+
+  if (wedding == null) {
     return null
   }
-  const{date} =wedding
+
+  const { date, galleryImages } = wedding
 
   return (
     <div className={cx('container')}>
-      <Heading date={date}/>
-      <Video/>
-
+      <Heading date={date} />
+      <Video />
+      <ImageGallery images={galleryImages} />
       {JSON.stringify(wedding)}
     </div>
   )
