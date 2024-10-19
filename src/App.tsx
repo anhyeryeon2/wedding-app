1a66183425e20489bf4c052417171a2a88f1e65d
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-10-20 01:38:28 +0900

    add ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import classNames from 'classnames/bind'
-import { useEffect, useState } from 'react'
 
 import styles from './App.module.scss'
 import Heading from './components/sections/Heading'
 import Video from './components/sections/Video'
 
-import FullScreenMessage from './components/shared/FullScreenMessage'
-import { Wedding } from '@models/wedding'
 import ImageGallery from './components/sections/ImageGallery'
 import Intro from './components/sections/Intro'
 import Invitation from './components/sections/Invitation'
@@ -19,12 +16,7 @@ import useWedding from './hooks/useWedding'
 const cx = classNames.bind(styles)
 
 function App() {
-  const { wedding, isLoading, error } = useWedding()
-
-
-  if (error) {
-    return <FullScreenMessage type="error" />
-  }
+  const { wedding } = useWedding()
 
   if (wedding == null) {
     return null
